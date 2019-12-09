4590176477d743ec913623ac5d528aa7026a66f1
Daniel Castro Jara - Ceiba Software <daniel.castro@ceiba.com.co>
2019-12-09 10:34:29 -0500

    Adding services

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  friends: User [];
+  constructor() { 
+    let user1: User = {
+      nick:'user',
+      subnick:'1',
+      age:22,
+      email:'user@gmail.com',
+      friend:true,
+      uid: 1
+    };
+    let user2: User = {
+      nick:'carl',
+      subnick:'1',
+      age:19,
+      email:'carl@gmail.com',
+      friend:true,
+      uid: 2
+    };
+    let user3: User = {
+      nick:'Juan',
+      subnick:'1',
+      age:25,
+      email:'juan@gmail.com',
+      friend:false,
+      uid: 3
+    };
+    let user4: User = {
+      nick:'Ben',
+      subnick:'1',
+      age:19,
+      email:'ben@gmail.com',
+      friend:false,
+      uid: 4
+    };
+    let user5: User = {
+      nick:'Jhon',
+      subnick:'1',
+      age:30,
+      email:'carl@gmail.com',
+      friend:true,
+      uid: 5
+    };
+    this.friends = [user1,user2,user3,user4,user5];
+  }
+  getFriends(){
+    return this.friends;
+  }
+}
