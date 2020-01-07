43473c62736d29a0318a24486e9ab1af38862f5d
Daniel Castro Jara - Ceiba Software <daniel.castro@ceiba.com.co>
2020-01-07 16:55:18 -0500

    adding storage to database

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,7 +3,8 @@ import { User } from '../interfaces/user';
 import { UserService } from '../services/user.service';
 import { AuthenticationService } from '../services/authentication.service';
 import { ImageCroppedEvent } from 'ngx-image-cropper';
-import { FirebaseStorage } from 'angularfire2/storage';
+//-import { FirebaseStorage } from 'angularfire2/storage';
+import { FirebaseStorage } from '@firebase/storage-types';
 
 @Component({
   selector: 'app-profile',
